e019428748b7e15e69836aa06ea06a38bb586ecd
deb abarzav <abarcazavalad@gmail.com>
2025-02-23 21:27:27 -0500

    refactor: :recycle: Replace Header section with Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Header from "./components/Header"
+import Header from "./components/Navbar"
 import Hero from "./components/Hero"
 
 function App() {
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="nav-brand">DeJota</div>
+      <div className="nav-links">
+        <a href="#experiencia">Experiencia</a>
+        <a href="#habilidades">Habilidades</a>
+        <a href="#proyectos">Proyectos</a>
+        <div className="theme-toggle">🌙</div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
