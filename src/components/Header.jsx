2b01ab028b87af443954e47dc68f89427b77b27e
deb abarzav <abarcazavalad@gmail.com>
2025-02-23 20:13:51 -0500

    feat: :sparkles: Added the structure of the header section

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>Mi Portafolio</h1>
+      <nav>
+        <a href="#experiencia">Experiencia</a>
+        <a href="#habilidades">Habilidades</a>
+        <a href="#proyectos">Proyectos</a>
+        <a href="#contacto">Contacto</a>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
