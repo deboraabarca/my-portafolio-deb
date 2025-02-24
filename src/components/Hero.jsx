cc05a3002e267d99429f93775565816dc13229c3
deb abarzav <abarcazavalad@gmail.com>
2025-02-23 20:36:20 -0500

    feat: :sparkles: Added the structure of the hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+
+const Hero = () => {
+  return (
+    <section className="hero">
+      <div className="hero-content">
+        <h2>Me gusta hacer cosas <span className="highlight">dinámicas</span> e interactivas con código.</h2>
+        <button className="btn">Ver proyectos</button>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
