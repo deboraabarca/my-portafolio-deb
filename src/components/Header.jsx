ad70933fa4333433ef6cd34923ef0e6387a148b9
deb abarzav <abarcazavalad@gmail.com>
2025-02-24 21:19:49 -0500

    refactor: :recycle: replace Navbar component with Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+function Header() {
+  return (
+    <header className="header">
+      <div className="header-container">
+        <div className="logo">Abeiro</div>
+        <nav className="nav">
+          <ul>
+            <li><a href="#experiencia">Experiencia</a></li>
+            <li><a href="#habilidades">Habilidades</a></li>
+            <li><a href="#proyectos">Proyectos</a></li>
+            <li><a href="#contacto">Contacto</a></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
