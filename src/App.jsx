678371f459b4a8096a857d5b67e40da1d37b1e47
deb abarzav <abarcazavalad@gmail.com>
2025-03-02 23:51:25 -0500

    feat: :sparkles: add structure for the experience section

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 
+import Experience from "./components/Experience"
 import Header from "./components/Header"
 
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <div>
       <Header/>
+      <Experience/>
     </div>
   )
 }
---
+++ src/components/Experience.jsx
@@ -0,0 +1,64 @@
+
+
+const Experience = () => {
+  return (
+    <div className="experience-container">
+      <h1 className="experience-title">Experiencia</h1>
+
+      <div className="experience-item">
+        <div className="experience-company">
+          <h3>Easy Hotel</h3>
+          <div className="underline"></div>
+        </div>
+        <div className="experience-details">
+          <h2>Fronted Developer Intern</h2>
+          <p className="experience-date">(Febrero 2024-Julio-2024)</p>
+          <p className="experience-description">
+            Desarrollé habilidades en HTML5, CSS, JavaScript y React, 
+            con el objetivo de reforzar mis conocimientos y crear 
+            proyectos personales que me permitan aplicar lo aprendido 
+            y seguir mejorando en el desarrollo web.
+          </p>
+        </div>
+      </div>
+
+      <div className="experience-item">
+        <div className="experience-company">
+          <h3>Biker Services</h3>
+          <div className="underline"></div>
+        </div>
+        <div className="experience-details">
+          <h2>Frontend Developer Intern</h2>
+          <p className="experience-date">(Agosto 2024-Diciembre 2024)</p>
+          <p className="experience-description">
+            Desarrollé y diseñé interfaces web utilizando HTML5, CSS, 
+            JavaScript y React, creando soluciones responsivas y 
+            visualmente atractivas. Colaboré con equipos de diseño 
+            para implementar prototipos en Figma
+          </p>
+        </div>
+      </div>
+
+      <div className="experience-item">
+        <div className="experience-company">
+          <h3>Digital Buho</h3>
+          <div className="underline"></div>
+        </div>
+        <div className="experience-details">
+          <h2>Frontend Developer Intern</h2>
+          <p className="experience-date">(Agosto 2024-Diciembre 2024)</p>
+          <p className="experience-description">
+            Desarrollo y diseño interfaces web utilizando HTML5, 
+            CSS, JavaScript y React, creando soluciones 
+            responsivas y visualmente atractivas. Colaboro con 
+            equipos de diseño para implementar prototipos en 
+            Figma. Aplico buenas prácticas de desarrollo web para 
+            optimizar la experiencia del usuario.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Experience;
