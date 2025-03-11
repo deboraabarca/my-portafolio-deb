f73fd5049843f06375ff41f1a5b6c899dd6e3af3
deb abarzav <abarcazavalad@gmail.com>
2025-03-10 20:38:40 -0500

    style: :lipstick: improve styles in Contact, Experience, Header, Projects, and Skills

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,29 @@
-
-import Header from './components/Header'
-import Experience from './components/Experience'
-import Skills from './components/Skills'
-import Proyects from './components/Proyects'
-import Contact from './components/Contact'
-import Scrolltotop from './components/Scrolltoptop'
+import Contact from "./components/Contact";
+import Experience from "./components/Experience";
+import Header from "./components/Header";
+import Proyects from "./components/Proyects";
+import Scrolltotop from "./components/Scrolltoptop";
+import Skills from "./components/Skills";
 
 function App() {
-  return (
-    <div>
-      <Header />
-      <section id="experience">
-        <Experience />
-      </section>
-      <section id="skills">
-        <Skills />
-      </section>
-      <section id="projects">
-        <Proyects />
-      </section>
-      <section id="contact">
-        <Contact />
-      </section>
-      <Scrolltotop />
-    </div>
-  )
+	return (
+		<div>
+			<Header />
+			<section id="experience">
+				<Experience />
+			</section>
+			<section id="skills">
+				<Skills />
+			</section>
+			<section id="projects">
+				<Proyects />
+			</section>
+			<section id="contact">
+				<Contact />
+			</section>
+			<Scrolltotop />
+		</div>
+	);
 }
 
-export default App
+export default App;
