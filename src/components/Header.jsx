0016ab0bda5693ad59e11e68c4ddc888d98dbbe3
deb abarzav <abarcazavalad@gmail.com>
2025-03-12 01:45:59 -0500

    feat: :sparkles: update download cv

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -72,7 +72,11 @@ const Header = () => {
 						<h1 className="headline">e interactivas con código.</h1>
 					</div>
 
-					<a href="/public/cv.pdf" download className="download-button">
+					<a
+						href="https://github.com/deboraabarca/my-portafolio-deb/blob/main/public/cv.pdf"
+						download
+						className="download-button"
+					>
 						Descargar CV
 					</a>
 				</div>
