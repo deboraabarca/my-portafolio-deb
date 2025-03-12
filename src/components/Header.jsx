829a691628b43bc64f351b653f4ccc7e1d8c21bf
deb abarzav <abarcazavalad@gmail.com>
2025-03-11 19:44:19 -0500

    refactor: :recycle: (refactor structure and style for the header section)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,11 +11,8 @@ const Header = () => {
 	return (
 		<div className="portfolio-container">
 			<div className="portfolio-left">
-				<div className="logo">Débora</div>
+				<div className="logo">Debora</div>
 			</div>
-		<div className="workspace-image">
-  <img src={workspaceImage} alt="Workspace" />
-  </div>
 
 			<div className="portfolio-right">
 				<nav className="navigation">
@@ -25,8 +22,7 @@ const Header = () => {
 								Experiencia
 							</a>
 							<a href="#experience" className="mobile-nav">
-								<FaBriefcase />
-								<span>Experiencia</span>
+								<FaBriefcase /> <span>Experiencia</span>
 							</a>
 						</li>
 						<li>
@@ -34,8 +30,7 @@ const Header = () => {
 								Habilidades
 							</a>
 							<a href="#skills" className="mobile-nav">
-								<FaCode />
-								<span>Habilidades</span>
+								<FaCode /> <span>Habilidades</span>
 							</a>
 						</li>
 						<li>
@@ -43,8 +38,7 @@ const Header = () => {
 								Proyectos
 							</a>
 							<a href="#projects" className="mobile-nav">
-								<FaProjectDiagram />
-								<span>Proyectos</span>
+								<FaProjectDiagram /> <span>Proyectos</span>
 							</a>
 						</li>
 						<li>
@@ -52,32 +46,36 @@ const Header = () => {
 								Contáctame
 							</a>
 							<a href="#contact" className="mobile-nav">
-								<FaEnvelope />
-								<span>Contáctame</span>
+								<FaEnvelope /> <span>Contáctame</span>
 							</a>
 						</li>
 					</ul>
 				</nav>
 
 				<div className="main-content">
-  <div className="greeting">
-    <span className="wave-emoji">👋</span>
-    <span>Hola <span className="soy">soy</span> <span className="name">Débora</span></span>
-  </div>
+					<div className="workspace-image">
+						<img src={workspaceImage} alt="Workspace" />
+					</div>
+					<div className="presentation">
+						<span className="wave-emoji">👋</span>
+						<span>
+							Hola <span className="soy">soy</span>{" "}
+							<span className="name">Debora</span>
+						</span>
+					</div>
 
-  <div className="headline-container">
-    <h1 className="headline">
-      Me gusta hacer <span className="highlight">cosas</span>
-      <span className="transparent">divertidas</span>
-    </h1>
-    <h1 className="headline">e interactivas</h1>
-    <h1 className="headline">con código.</h1>
-  </div>
+					<div className="headline-container">
+						<h1 className="headline">
+							Me gusta hacer <span className="highlight">cosas</span>
+							<span className="transparent"> divertidas</span>
+						</h1>
+						<h1 className="headline">e interactivas con código.</h1>
+					</div>
 
-  <a href="/public/cv.pdf" download className="download-button">
-    Descargar CV
-  </a>
-</div>
+					<a href="/public/cv.pdf" download className="download-button">
+						Descargar CV
+					</a>
+				</div>
 			</div>
 		</div>
 	);
